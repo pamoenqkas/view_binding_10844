23be64e061b63f94ca019d1d2009a7477c6aa18f
pamoenqkas <pamoenqkas@gmail.com>
2022-09-13 14:56:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10844/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10844
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan sehari-hari untuk membuat hari makni Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha Mama Yaemiko tercinga / Keging Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Ngulis, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke TIktok",
+                "Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+}
